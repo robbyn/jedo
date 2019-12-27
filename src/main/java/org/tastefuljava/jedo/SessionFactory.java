d758074c305503ef068adad4d9d1c7b8993b86c8
Maurice Perry <maurice@perry.productions>
2019-12-27 18:51:20 +0100

    Added SessionFactory

---
+++ src/main/java/org/tastefuljava/jedo/SessionFactory.java
@@ -0,0 +1,30 @@
+package org.tastefuljava.jedo;
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+import org.tastefuljava.jedo.mapping.Mapper;
+
+public class SessionFactory {
+    public static final Logger LOG
+            = Logger.getLogger(SessionFactory.class.getName());
+
+    private final DataSource ds;
+    private final Mapper mapper;
+
+    public SessionFactory(DataSource ds, Mapper mapper) {
+        this.ds = ds;
+        this.mapper = mapper;
+    }
+
+    public Session openSession() {
+        try {
+            return new Session(ds.getConnection(), mapper);
+        } catch (SQLException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+            throw new JedoException("Cannot connect to database: "
+                    + ex.getMessage());
+        }
+    }
+}
